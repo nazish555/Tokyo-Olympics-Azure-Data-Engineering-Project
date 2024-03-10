68df76ae1a5841d0c6c880d9fa6ac9ec50d5c992
Nazish Mehmood <74066356+nazish555@users.noreply.github.com>
2024-03-10 20:33:33 +0500

    Create Analysis_Scripts.sql

---
+++ Analysis_Scripts.sql
@@ -0,0 +1,22 @@
+--count the number of athletes from each country
+
+SELECT Country, count(*) as athlete_count
+from Athlete
+GROUP by Country
+order by athlete_count desc;
+
+---Calculate the total medals won by each country
+
+select TeamCountry,
+sum(Gold) as Total_Gold,
+sum(Silver) as Total_Silver,
+sum(Bronze) as Total_Bronze
+from medals
+group by TeamCountry;
+
+
+---Calculate the avg number of enteries by gender for each discipline
+SELECT Discipline, avg(Male) as avg_male,
+avg(Female) as avg_female
+from entriesgender 
+group by Discipline
